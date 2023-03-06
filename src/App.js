2249db2e3e46237db48142f6f41c0b4d6d8d43a5
Tom Grant <grntthomasronnie@gmail.com>
2023-03-06 21:34:50 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [items, setItems] = useState([
+    "Walk Dog",
+    "Milk Cows",
+    "Shower",
+    "Sleep"
+  ])
+
+  const [newItemName, setNewItemName] = useState("");
+
+  const itemNodes = items.map((item, index) => {
+    return <li key = {index}>
+             <span>{item}</span>
+           </li>
+  });
+
+  function saveNewItem(event) {
+    event.preventDefault();
+    console.log(newItemName);
+    const newItem = newItemName;
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setNewItemName("")
+  }
+
+  function handelInputChange(event) {
+    setNewItemName(event.target.value);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ToDo's</h1>
+
+      <form onSubmit={saveNewItem}>
+      <input type="text" placeholder='Enter Task Name' onChange={handelInputChange}></input>
+      <label value="High" id='high'>High</label>
+      <input type="radio" id='high' value="High"></input>
+      <label value="Low" id="low">Low</label>
+      <input type="radio" id='low' value="Low"></input>
+      <input type="submit" value="Add New Item"></input>
+      </form>
+
+    <ul>
+      {itemNodes}
+    </ul>
+
+
+
     </div>
   );
 }
